80f6cf9568480c9ccf0e4252fc044bf3cd18b0ed
Maes Hanne <hannemaes@bapitzemburg.be>
2023-01-11 10:46:52 +0100

    now works recursive for all filetypes

--- batchImageDiff.py
+++ batchImageDiff.py
@@ -1,60 +1,65 @@
 import os
-from PIL import Image
 import hashlib
 
-counter = 0
 namesAndHashes = []
 
+# Get all files in the current dir and all subdirs, that are not hidden
+for dirpath, dirnames, filenames in os.walk("."):
+    for dirname in dirnames:
+        if dirname.startswith("."):
+            dirnames.remove(dirname)
+    for filename in filenames:
+        if not filename.startswith("."):
+
+            # Exclude the script files
+            if filename != "batchImageDiff.py" and filename != "LICENSE" and filename != "README.md":
+
+                # Found a file that is not hidden in a dir that's not hidden
+                filePath = os.path.join(dirpath, filename)
+
+                # Hash the file
+                h = hashlib.sha256() # make a hash object
+                with open(filePath,'rb') as file: # open file for reading in binary mode
+                    chunk = 0
+                    while chunk != b'': # loop till the end of the file
+                        chunk = file.read(1024) # read only 1024 bytes at a time
+                        h.update(chunk)
+                hashedFile = h.hexdigest() # return the hex representation of digest
+                    
+                # Add to array
+                namesAndHashes.append([filePath, hashedFile])
+
+# Print all files
 print()
-
-# Get all images in the current dir
-extensions = ('.jpeg', '.jpg', '.png', '.gif', '.bmp', '.JPEG', '.JPG', '.PNG', '.GIF', '.BMP') # Define the extension of image files
-current_dir = os.getcwd() # Get the current directory
-for filename in os.listdir(current_dir): # Loop over all files in the directory
-    if filename.endswith(extensions): # Check if the file is an image
-        
-        counter = counter + 1
-
-        # hash image
-        with Image.open(filename) as im:
-            binary_data = im.tobytes() # Convert the image to a binary string
-            hash_object = hashlib.sha256() # Create a new SHA-256 hash object     
-            hash_object.update(binary_data) # Update the hash object with the binary data
-            image_hash = hash_object.hexdigest() # Get the hexadecimal representation of the hash
-
-            # Add to array
-            namesAndHashes.append([filename, image_hash])
+print('Checking these ' + str(len(namesAndHashes)) + ' files:')
+for i in namesAndHashes:
+    print('   ➡️ ' + i[0])
+print()
 
 # Sort array
 namesAndHashes.sort(key=lambda namesAndHashes:namesAndHashes[1]) 
 
-# print the whole list
-print('Checking these ' + str(counter) + ' images:')
-for i in range(0, counter):
-    print('   ➡️  ' + namesAndHashes[i][0])
-print()
-
 # Compare hashes
-prevIsDifferent = False # true means that the prev loop iteration found a double
-for i in range(0, counter-1): # counter-1 because I check with the next image, and the next image of the last image doesn't exist
+prevIsDifferent = False # True means that the prev loop iteration found a double
+for i in range(0, len(namesAndHashes)-1): # len(namesAndHashes)-1 because I check with the next image, and the next image of the last image doesn't exist
 
-    if prevIsDifferent == False:
-
-        if namesAndHashes[i][1] == namesAndHashes[i+1][1]:
-            print('❌ ' + namesAndHashes[i][1] + ' ➡️  ' + namesAndHashes[i][0])
-            prevIsDifferent = True
-        else:
-            print('🟢 ' + namesAndHashes[i][1])
-            prevIsDifferent = False
-    
+    # Check if this file is the same as the next one
+    if namesAndHashes[i][1] == namesAndHashes[i+1][1]:
+        print('❌ ' + namesAndHashes[i][1] + ' ➡️ ' + namesAndHashes[i][0])
+        prevIsDifferent = True
     else:
-        print('❌ ' + namesAndHashes[i][1] + ' ➡️  ' + namesAndHashes[i][0])
+        # If the prev file was flagged as the same we always need a ❌, but we also need to check againt the next file
+        if prevIsDifferent == False:
+            print('🟢 ' + namesAndHashes[i][1])
+        else:
+            print('❌ ' + namesAndHashes[i][1] + ' ➡️ ' + namesAndHashes[i][0])
+        prevIsDifferent = False
 
-# the last image was already checked in the loop
-lastItem = counter - 1
-if prevIsDifferent == False:
-    print('❌ ' + namesAndHashes[lastItem][1] + ' ➡️  ' + namesAndHashes[lastItem][0])
-else:
+# Last image was already checked in the loop
+lastItem = len(namesAndHashes) - 1
+if prevIsDifferent == False: # If the prev file was flagged as the same we always need a ❌, but we also need to check againt the next file
     print('🟢 ' + namesAndHashes[lastItem][1])
+else:
+    print('❌ ' + namesAndHashes[lastItem][1] + ' ➡️ ' + namesAndHashes[lastItem][0])
 
 print()
